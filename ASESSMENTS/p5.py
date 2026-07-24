24d55b46d3b2ae53422f07e2e0f8f57501ac8a40
poornateja-88 <poornatejachinni@gmail.com>
2026-07-24 16:41:05 +0530

    Delete ASESSMENTS directory

--- ASESSMENTS/p5.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-import matplotlib
-import sklearn
-
-print("NumPy:", np.__version__)
-print("Pandas:", pd.__version__)
-print("Matplotlib:", matplotlib.__version__)
-print("Scikit-learn:", sklearn.__version__)
--- ASESSMENTS/p6.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-a = torch.tensor([1, 2, 3])
-b = torch.tensor([4, 5, 6])
-
-print("Addition:", a + b)
-print("Subtraction:", a - b)
-print("Multiplication:", a * b)
