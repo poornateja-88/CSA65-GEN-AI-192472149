e58f9c0ed05bd50fe6feaceb98de57fe62d64736
poornateja-88 <poornatejachinni@gmail.com>
2026-07-24 16:45:35 +0530

    Add files via upload

---
+++ UNIT 1 LAB PROGRAMS/p4.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+data = {
+    "Name": ["Arun", "Priya", "Rahul"],
+    "Grade": [85, 90, 78]
+}
+
+df = pd.DataFrame(data)
+
+print(df)
---
+++ UNIT 1 LAB PROGRAMS/p5.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import matplotlib
+import sklearn
+
+print("NumPy:", np.__version__)
+print("Pandas:", pd.__version__)
+print("Matplotlib:", matplotlib.__version__)
+print("Scikit-learn:", sklearn.__version__)
